ac347cc8655a4191f9d8bad9405709a234d45676
KamillKAPLAN <kamilkaplnn@gmail.com>
2020-05-11 04:02:30 +0300

    InputSelect kullanımı incelendi.

--- 01_BlazorLearn/BlazorLearn/Models/ProductAddModel.cs
+++ 01_BlazorLearn/BlazorLearn/Models/ProductAddModel.cs
@@ -12,8 +12,13 @@ namespace BlazorLearn.Models
         [Required(ErrorMessage = "productName zorunludur.")]
         [StringLength(20, ErrorMessage = "productName karakter sayısını aştı(Max 20)")]
         public string ProductName { get; set; }
+
         [Required(ErrorMessage = "description zorunludur.")]
         [StringLength(40, ErrorMessage = "description karakter sayısını aştı(Max 40)")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "category zorunludur.")]
+        public string CategoryId { get; set; }
+
     }
 }
