893ddc253f40e48531db4c55e17d26cf232b960c
KamillKAPLAN <kamilkaplnn@gmail.com>
2020-05-12 06:50:17 +0300

    post service'imiz ve interface'imiz oluşturuldu.

---
+++ 02_BlazorProject/BlazorProject/Services/IPostService.cs
@@ -0,0 +1,16 @@
+﻿using _02_BlazorProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorNorthwindUI.Services
+{
+    interface IPostService
+    {
+        Task<PostListViewModel[]> GetPosts();
+        Task<PostListViewModel> GetPostById(int postId);
+        Task PostCreate(PostListViewModel postListViewModel); // Add
+        Task PostUpdate(PostListViewModel postListViewModel); // Save
+    }
+}
