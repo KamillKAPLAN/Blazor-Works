bfed7f3ffd285fcccaa0319d4965a662b8930843
KamillKAPLAN <kamilkaplnn@gmail.com>
2020-05-12 06:28:59 +0300

    post modelimiz oluşturuldu

---
+++ 02_BlazorProject/02_BlazorProject/Models/PostListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _02_BlazorProject.Models
+{
+    public class PostListViewModel
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+    }
+}
