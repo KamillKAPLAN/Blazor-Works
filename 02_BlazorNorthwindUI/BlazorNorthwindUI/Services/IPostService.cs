878a4f79321d5034ce06e3399406a08d0d0fe5c0
KamillKAPLAN <kamilkaplnn@gmail.com>
2020-05-12 06:23:44 +0300

    blazor proje dosyası oluşturuldu

---
+++ 02_BlazorNorthwindUI/BlazorNorthwindUI/Services/IPostService.cs
@@ -0,0 +1,17 @@
+﻿using BlazorNorthwindUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorNorthwindUI.Services
+{
+    interface IPostService
+    {
+        Task<PostListViewModel[]> GetPosts();
+        Task<PostListViewModel> GetPostById(int postId);
+        Task PostCreate(PostListViewModel postListViewModel); // Add
+        Task PostUpdate(PostListViewModel postListViewModel); // Save
+
+    }
+}
---
+++ 02_BlazorNorthwindUI/BlazorNorthwindUI/Services/PostService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BlazorNorthwindUI.Models;
+
+namespace BlazorNorthwindUI.Services
+{
+    public class PostService : IPostService /* IPostService'i implemente ediyoruz. */
+    {
+        private HttpClient _httpClient;
+
+        public PostService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<PostListViewModel> GetPostById(int postId)
+        {
+            return await _httpClient.GetJsonAsync<PostListViewModel[]>("https://jsonplaceholder.typicode.com/posts/" + postId);
+        }
+
+        public async Task<PostListViewModel[]> GetPosts()
+        {
+            return await _httpClient.GetJsonAsync<PostListViewModel[]>("https://jsonplaceholder.typicode.com/posts");
+        }
+
+        public async Task PostCreate(PostListViewModel postListViewModel)
+        {
+            await _httpClient.PostJsonAsync("https://jsonplaceholder.typicode.com/posts", postListViewModel);
+        }
+
+        public async Task PostUpdate(PostListViewModel postListViewModel)
+        {
+            await _httpClient.PostJsonAsync("https://jsonplaceholder.typicode.com/posts", postListViewModel);
+        }
+    }
+}
