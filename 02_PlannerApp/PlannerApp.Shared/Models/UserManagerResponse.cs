cb4c52182bbf39c6fb834fc5568775352821ed21
KamillKAPLAN <kamilkaplnn@gmail.com>
2020-05-14 04:02:28 +0300

    AlertMessage Shared component

--- 02_PlannerApp/PlannerApp.Shared/Models/UserManagerResponse.cs
+++ 02_PlannerApp/PlannerApp.Shared/Models/UserManagerResponse.cs
@@ -9,14 +9,6 @@ namespace PlannerApp.Shared.Models
         public string Message { get; set; }
         public bool IsSuccess { get; set; }
         public string[] Errors { get; set; }
-        public UserInfo UserInfo { get; set; }
         public DateTime? ExpireDate { get; set; }
-    }
-
-    public class UserInfo
-    {
-        public string AdditionalProp1 { get; set; }
-        public string AdditionalProp2 { get; set; }
-        public string AdditionalProp3 { get; set; }
     }
 }
